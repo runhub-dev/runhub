c7ffca2168e21df25968b528101bc6523cf850c3
Michael Gallagher <mjgallag@gmail.com>
2024-01-18 12:43:19 -0800

    feat: add install direnv script

---
+++ scripts/install-direnv.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+version='2.33.0'
+
+is_installed() {
+  command -v direnv > /dev/null 2>&1
+}
+
+is_installed_with_devbox_global() {
+  direnv_path="$(command -v direnv)"
+  devbox_global_path="$(devbox global path 2> /dev/null)"
+  [ "${direnv_path}" != "${direnv_path#"${devbox_global_path}"}" ]
+}
+
+get_version() {
+  direnv version
+}
+
+is_minimum_required_version() {
+  "$(dirname "$0")"/is-version-greater-equal.sh "$(get_version)" "${version}"
+}
+
+append_if_not_found() {
+  if ! [ -f "$2" ] || ! grep -Fq "$1" "$2"; then
+    printf '\n%s\n' "$1" >> "$2"
+  fi
+}
+
+install() {
+  output="$(devbox global add direnv@"${version}" 2>&1)" || exit_status="$?"
+
+  if [ "${exit_status:-0}" != 0 ]; then
+    "$(dirname "$0")"/print.sh "${output}"
+    exit "${exit_status}"
+  fi
+
+  eval "$(devbox global shellenv)"
+  devbox_global_bin_path="$(dirname "$(command -v direnv)")"
+  append_if_not_found 'PATH='"${devbox_global_bin_path}"':"${PATH}"' ~/.bashrc
+  append_if_not_found 'PATH='"${devbox_global_bin_path}"':"${PATH}"' ~/.zshrc
+  append_if_not_found 'eval "$(direnv hook bash)"' ~/.bashrc
+  append_if_not_found 'eval "$(direnv hook zsh)"' ~/.zshrc
+  "$(dirname "$0")"/print.sh 'Restart shell to activate direnv.'
+}
+
+update() {
+  if ! is_installed_with_devbox_global; then
+    "$(dirname "$0")"/print.sh \
+      'direnv cannot be updated because it was not installed with Devbox Global, either uninstall it or update it.'
+    exit 1
+  fi
+
+  install
+}
+
+if ! is_installed; then
+  "$(dirname "$0")"/confirm.sh 'direnv not found, install with Devbox Global?'
+  install
+elif ! is_minimum_required_version; then
+  "$(dirname "$0")"/confirm.sh 'direnv outdated, update to v'"${version}"' with Devbox Global?'
+  update
+fi
