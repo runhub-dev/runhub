9b3c9400f7b0bc05c46fbc0b330da875a8d78dc5
Michael Gallagher <mjgallag@gmail.com>
2024-01-19 07:54:57 -0800

    fix: update devbox if launcher outdated

--- scripts/install-devbox.sh
+++ scripts/install-devbox.sh
@@ -4,17 +4,26 @@ set -o errexit
 set -o nounset
 
 version='0.8.5'
+launcher_version='0.2.1'
 
 is_installed() {
   command -v devbox > /dev/null 2>&1
 }
 
 get_version() {
-  devbox version --quiet
+  devbox version --quiet --verbose | grep '^Version:' | tr -s ' ' | cut -d ' ' -f 2
+}
+
+get_launcher_version() {
+  devbox version --quiet --verbose | grep '^Launcher:' | tr -s ' ' | cut -d ' ' -f 2
 }
 
 is_minimum_required_version() {
   "$(dirname "$0")"/is-version-greater-equal.sh "$(get_version)" "${version}"
+}
+
+is_minimum_required_launcher_version() {
+  "$(dirname "$0")"/is-version-greater-equal.sh "$(get_launcher_version)" "${launcher_version}"
 }
 
 install() {
@@ -28,7 +37,7 @@ update() {
 if ! is_installed; then
   "$(dirname "$0")"/confirm.sh 'Devbox not found, install?'
   install
-elif ! is_minimum_required_version; then
+elif ! is_minimum_required_version || ! is_minimum_required_launcher_version; then
   "$(dirname "$0")"/confirm.sh 'Devbox outdated, update?'
   update
 fi
