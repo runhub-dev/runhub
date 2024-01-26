c679c889dd0d906fa63edef1734a0a13460dac27
Michael Gallagher <mjgallag@gmail.com>
2024-01-25 18:02:15 -0800

    fix: exit with error if grep exit status >1
    https://pubs.opengroup.org/onlinepubs/9699919799/utilities/grep.html

--- scripts/append-if-not-found.sh
+++ scripts/append-if-not-found.sh
@@ -3,8 +3,20 @@
 set -o errexit
 set -o nounset
 
+append() {
+  printf '\n%s\n' "$1" >> "$2"
+}
+
 is_found="$("$(dirname "$0")"/is-found.sh "$2")"
 
-if [ "${is_found}" = 'no' ] || ! grep -Fq "$1" "$2"; then
-  printf '\n%s\n' "$1" >> "$2"
+if [ "${is_found}" = 'yes' ]; then
+  grep -Fq "$1" "$2" || exit_status="$?"
+
+  if [ "${exit_status:-0}" = 1 ]; then
+    append "$@"
+  else
+    exit "${exit_status:-0}"
+  fi
+else
+  append "$@"
 fi
