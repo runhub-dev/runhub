b799cbaef209414d3767424e0ed30200a8b943c4
Michael Gallagher <mjgallag@gmail.com>
2024-02-05 11:16:52 -0800

    fix: remove local dev cluster from kubeconfig on stop

--- scripts/stop-local-dev-cluster.sh
+++ scripts/stop-local-dev-cluster.sh
@@ -14,3 +14,8 @@ if k3d cluster get dev-runhub > /dev/null 2>&1; then
   echo 'Stopping local dev Kubernetes cluster in Docker.'
   k3d cluster delete dev-runhub
 fi
+
+kubectl config unset current-context > /dev/null
+kubectl config delete-context k3d-dev-runhub > /dev/null 2>&1 || true
+kubectl config delete-cluster k3d-dev-runhub > /dev/null 2>&1 || true
+kubectl config delete-user admin@k3d-dev-runhub > /dev/null 2>&1 || true
