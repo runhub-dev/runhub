48ea450bfa55e4411eba235f20d2d7e9d3940f6d
Michael Gallagher <mjgallag@gmail.com>
2024-02-05 07:55:27 -0800

    refactor: inline is_colima_docker_daemon_running

--- scripts/stop-local-dev-cluster.sh
+++ scripts/stop-local-dev-cluster.sh
@@ -3,28 +3,14 @@
 set -o errexit
 set -o nounset
 
-is_colima_docker_daemon_running() {
-  colima_docker_daemon="$(colima list --json --profile dev-runhub)"
+is_colima_docker_daemon_running="$(colima list --json --profile dev-runhub)"
 
-  if [ "${colima_docker_daemon}" ]; then
-    echo 'yes'
-  else
-    echo 'no'
-  fi
-}
+if [ "${is_colima_docker_daemon_running}" ]; then
+  echo 'Stopping Colima Docker daemon and local dev Kubernetes cluster in Docker.'
+  colima delete --force --profile dev-runhub
+fi
 
-main() {
-  is_colima_docker_daemon_running="$(is_colima_docker_daemon_running)"
-
-  if [ "${is_colima_docker_daemon_running}" = 'yes' ]; then
-    echo 'Stopping Colima Docker daemon and local dev Kubernetes cluster in Docker.'
-    colima delete --force --profile dev-runhub
-  fi
-
-  if k3d cluster list dev-runhub > /dev/null 2>&1; then
-    echo 'Stopping local dev Kubernetes cluster in Docker.'
-    k3d cluster delete dev-runhub
-  fi
-}
-
-main "$@"
+if k3d cluster list dev-runhub > /dev/null 2>&1; then
+  echo 'Stopping local dev Kubernetes cluster in Docker.'
+  k3d cluster delete dev-runhub
+fi
