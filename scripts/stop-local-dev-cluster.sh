ffb604fcc4515d1f21920c2ccf6d9230c262d68b
Michael Gallagher <mjgallag@gmail.com>
2024-02-05 12:14:21 -0800

    refactor: always remove colima context from docker

--- scripts/stop-local-dev-cluster.sh
+++ scripts/stop-local-dev-cluster.sh
@@ -8,7 +8,6 @@ get_colima_docker_daemon="$(colima list --json --profile dev-runhub)"
 if [ "${get_colima_docker_daemon}" ]; then
   echo 'Stopping Colima Docker daemon and local dev Kubernetes cluster in Docker.'
   colima delete --force --profile dev-runhub
-  docker context rm --force colima-dev-runhub > /dev/null
 fi
 
 if k3d cluster get dev-runhub > /dev/null 2>&1; then
@@ -16,6 +15,7 @@ if k3d cluster get dev-runhub > /dev/null 2>&1; then
   k3d cluster delete dev-runhub
 fi
 
+docker context rm --force colima-dev-runhub > /dev/null
 kubectl config unset current-context > /dev/null
 kubectl config delete-context k3d-dev-runhub > /dev/null 2>&1 || true
 kubectl config delete-cluster k3d-dev-runhub > /dev/null 2>&1 || true
