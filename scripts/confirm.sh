0d0837c1f71e7cdd167d7f710013582fdbb8580f
Michael Gallagher <mjgallag@gmail.com>
2024-02-05 08:24:32 -0800

    refactor: remove if

--- scripts/confirm.sh
+++ scripts/confirm.sh
@@ -7,6 +7,4 @@ printf '%s ([y]es/[n]o): ' "$1"
 read -r yes_no < /dev/tty
 yes_no="$(echo "${yes_no}" | tr '[:upper:]' '[:lower:]')"
 
-if [ "${yes_no}" != 'y' ] && [ "${yes_no}" != 'yes' ]; then
-  exit 1
-fi
+[ "${yes_no}" = 'y' ] || [ "${yes_no}" = 'yes' ]
