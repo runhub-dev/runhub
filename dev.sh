13de5370cab7a3f454fa88fa89064f3ac219a026
Michael Gallagher <mjgallag@gmail.com>
2024-02-06 08:59:22 -0800

    fix: replace devbox run with direnv exec
    devbox run doesn't handle trap correctly

--- dev.sh
+++ dev.sh
@@ -6,31 +6,4 @@ set -o nounset
 RUNHUB_DIR="$(dirname "$0")"
 SCRIPTS_DIR="${RUNHUB_DIR}"/scripts
 
-devbox_run() {
-  devbox run --config "${RUNHUB_DIR}" "$@"
-}
-
-start() {
-  previous_docker_context="$(devbox_run docker context show)"
-  previous_kube_context="$(devbox_run kubectl config current-context 2> /dev/null || true)"
-  devbox_run "${SCRIPTS_DIR}"/start-local-dev-cluster.sh
-}
-
-stop() {
-  devbox_run "${SCRIPTS_DIR}"/stop-local-dev-cluster.sh
-  devbox_run docker context use "${previous_docker_context}" > /dev/null 2>&1 || true
-  devbox_run kubectl config use-context "${previous_kube_context}" > /dev/null 2>&1 || true
-}
-
-main() {
-  "${SCRIPTS_DIR}"/install-nix.sh
-  "${SCRIPTS_DIR}"/install-devbox.sh
-  "${SCRIPTS_DIR}"/install-direnv.sh
-  trap 'echo ; exit' INT
-  trap 'stop' EXIT
-  start
-  echo 'Press Ctrl+C to stop.'
-  sleep 2147483647
-}
-
-main "$@"
+direnv exec "${RUNHUB_DIR}" "${SCRIPTS_DIR}"/dev.sh
---
+++ scripts/dev.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+SCRIPTS_DIR="$(dirname "$0")"
+
+start() {
+  previous_docker_context="$(docker context show)"
+  previous_kube_context="$(kubectl config current-context 2> /dev/null || true)"
+  "${SCRIPTS_DIR}"/start-local-dev-cluster.sh
+}
+
+stop() {
+  "${SCRIPTS_DIR}"/stop-local-dev-cluster.sh
+  docker context use "${previous_docker_context}" > /dev/null 2>&1 || true
+  kubectl config use-context "${previous_kube_context}" > /dev/null 2>&1 || true
+}
+
+main() {
+  "${SCRIPTS_DIR}"/install-nix.sh
+  "${SCRIPTS_DIR}"/install-devbox.sh
+  "${SCRIPTS_DIR}"/install-direnv.sh
+  trap 'echo ; exit' INT
+  trap 'stop' EXIT
+  start
+  echo 'Press Ctrl+C to stop.'
+  sleep 2147483647
+}
+
+main "$@"
