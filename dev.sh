67d51139a727283913acfaf510874af9ca60dba7
Michael Gallagher <mjgallag@gmail.com>
2024-02-05 16:24:43 -0800

    refactor add devbox_run function to dev script

--- dev.sh
+++ dev.sh
@@ -6,18 +6,22 @@ set -o nounset
 RUNHUB_DIR="$(dirname "$0")"
 SCRIPTS_DIR="${RUNHUB_DIR}"/scripts
 
+devbox_run() {
+  devbox run --config "${RUNHUB_DIR}" "$@"
+}
+
 start_dev_mode() {
-  previous_docker_context="$(devbox run docker context show)"
-  previous_kube_context="$(devbox run kubectl config current-context 2> /dev/null || true)"
+  previous_docker_context="$(devbox_run docker context show)"
+  previous_kube_context="$(devbox_run kubectl config current-context 2> /dev/null || true)"
   echo 'Starting dev mode.'
-  devbox run --config "${RUNHUB_DIR}" "${SCRIPTS_DIR}"/start-local-dev-cluster.sh
+  devbox_run "${SCRIPTS_DIR}"/start-local-dev-cluster.sh
 }
 
 stop_dev_mode() {
   echo 'Stopping dev mode.'
-  devbox run --config "${RUNHUB_DIR}" "${SCRIPTS_DIR}"/stop-local-dev-cluster.sh
-  devbox run docker context use "${previous_docker_context}" > /dev/null 2>&1 || true
-  devbox run kubectl config use-context "${previous_kube_context}" > /dev/null 2>&1 || true
+  devbox_run "${SCRIPTS_DIR}"/stop-local-dev-cluster.sh
+  devbox_run docker context use "${previous_docker_context}" > /dev/null 2>&1 || true
+  devbox_run kubectl config use-context "${previous_kube_context}" > /dev/null 2>&1 || true
 }
 
 main() {
