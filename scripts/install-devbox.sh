3f7ed3478f9ffd3a1d644149a589e2335d88764f
Michael Gallagher <mjgallag@gmail.com>
2024-02-07 08:49:48 -0800

    perf: only call devbox once
    it is slow

--- scripts/install-devbox.sh
+++ scripts/install-devbox.sh
@@ -8,8 +8,7 @@ VERSION='0.9.1'
 LAUNCHER_VERSION='0.2.1'
 
 get_current_version() {
-  current_version_output="$(devbox version --quiet --verbose)"
-  current_version_grep="$(echo "${current_version_output}" | grep '^'"$1"':')"
+  current_version_grep="$(echo "$2" | grep '^'"$1"':')"
   current_version_tr="$(echo "${current_version_grep}" | tr -s ' ')"
   echo "${current_version_tr}" | cut -d ' ' -f 2
 }
@@ -24,9 +23,10 @@ main() {
     "${SCRIPTS_DIR}"/confirm.sh 'Devbox not found, install?'
     install
   else
-    current_version="$(get_current_version 'Version')"
+    current_version_output="$(devbox version --quiet --verbose)"
+    current_version="$(get_current_version 'Version' "${current_version_output}")"
     is_updated="$("${SCRIPTS_DIR}"/is-version-greater-equal.sh "${current_version}" "${VERSION}")"
-    current_launcher_version="$(get_current_version 'Launcher')"
+    current_launcher_version="$(get_current_version 'Launcher' "${current_version_output}")"
     is_launcher_updated="$("${SCRIPTS_DIR}"/is-version-greater-equal.sh \
       "${current_launcher_version}" "${LAUNCHER_VERSION}")"
 
