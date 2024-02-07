a3fda8b81cfb4e34031a6ff70bfe68da5d8d0b55
Michael Gallagher <mjgallag@gmail.com>
2024-02-07 10:42:28 -0800

    refactor: add direnv exec script

--- dev.sh
+++ dev.sh
@@ -6,13 +6,4 @@ set -o nounset
 RUNHUB_DIR="$(dirname "$0")"
 SCRIPTS_DIR="${RUNHUB_DIR}"/scripts
 
-"${SCRIPTS_DIR}"/install-nix.sh
-"${SCRIPTS_DIR}"/install-devbox.sh
-"${SCRIPTS_DIR}"/install-direnv.sh
-
-if ! command -v nix > /dev/null || ! command -v direnv > /dev/null; then
-  echo 'Restart shell and rerun to complete install and continue.'
-  exit 1
-fi
-
-direnv exec "${RUNHUB_DIR}" "${SCRIPTS_DIR}"/dev.sh
+"${SCRIPTS_DIR}"/direnv-exec.sh "${SCRIPTS_DIR}"/dev.sh
