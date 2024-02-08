1d548d5007342baf6781197b17895ff6459e95d4
Michael Gallagher <mjgallag@gmail.com>
2024-02-08 07:29:54 -0800

    Revert "style: remove newline"
    This reverts commit 47b128a09f65571468dc0a9913ec04e45799f372.

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -o errexit
-set -o nounset
-
-RUNHUB_DIR="$(dirname "$0")"
-SCRIPTS_DIR="${RUNHUB_DIR}"/scripts
-
-"${SCRIPTS_DIR}"/direnv-exec.sh "${SCRIPTS_DIR}"/install.sh
