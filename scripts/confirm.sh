8df3e5eb743acac1d99fc26aaa0d2c327166deb6
Michael Gallagher <mjgallag@gmail.com>
2024-02-07 17:44:21 -0800

    style: remove newline

--- scripts/confirm.sh
+++ scripts/confirm.sh
@@ -6,5 +6,4 @@ set -o nounset
 printf '%s ([y]es/[n]o): ' "$1"
 read -r yes_no < /dev/tty
 yes_no="$(echo "${yes_no}" | tr '[:upper:]' '[:lower:]')"
-
 [ "${yes_no}" = 'y' ] || [ "${yes_no}" = 'yes' ]
