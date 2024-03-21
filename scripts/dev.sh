22a615c7c1651396710b7f1338a40cb4d9fa7ead
Michael Gallagher <mjgallag@gmail.com>
2024-03-21 13:18:58 -0700

    refactor: rename runhub-dev-cluster to dev-runhub-cluster

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -124,7 +124,7 @@ start_dev_cluster() {
   k3d_version_output="$(k3d version --output json)"
   k3d_version="$(echo "${k3d_version_output}" | yq --exit-status '.k3d')"
   runub_absolute_dir="$(cd "${runhub_dir}" && pwd)"
-  dev_cluster_yaml="$(helm template "${runhub_dir}"/charts/runhub-dev-cluster \
+  dev_cluster_yaml="$(helm template "${runhub_dir}"/charts/dev-runhub-cluster \
     --set k3dVersion="${k3d_version}" --set runhubAbsoluteDir="${runub_absolute_dir}")"
 
   if k3d cluster get dev-runhub > /dev/null 2>&1; then
