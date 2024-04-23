6e5ae040c6de55bcfe85507cdbeac6da03ce5587
Michael Gallagher <mjgallag@gmail.com>
2024-04-23 11:57:24 -0700

    refactor: kubectl apply helm template like Argo CD

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -110,11 +110,10 @@ is_healthy() {
 
 install_operator() {
   echo 'Installing runhub operator.'
-  helm upgrade --install --create-namespace \
-    --namespace runhub runhub-operator \
+  operator_chart_yaml="$(helm template --namespace runhub runhub-operator \
     "${runhub_dir}"/charts/runhub-operator \
-    --set dev.repository='file:///runhub',dev.revision="${current_commit}" \
-    > /dev/null
+    --set dev.repository='file:///runhub',dev.revision="${current_commit}")"
+  echo "${operator_chart_yaml}" | kubectl apply --filename - > /dev/null
 }
 
 install() {
--- scripts/install-argo-cd.sh
+++ scripts/install-argo-cd.sh
@@ -7,16 +7,17 @@ scripts_dir="$(dirname "$0")"
 runhub_dir="${scripts_dir}"/..
 
 echo 'Installing Argo CD.'
-chart_yaml="$(helm template "${runhub_dir}"/charts/runhub \
+namespace_chart_yaml="$(helm template --namespace argocd argocd "${runhub_dir}"/charts/namespace)"
+runhub_chart_yaml="$(helm template --namespace runhub runhub "${runhub_dir}"/charts/runhub \
   --values "${runhub_dir}"/runhub-infra.yaml)"
-app_yaml="$(echo "${chart_yaml}" | yq --exit-status '
+app_source_yaml="$(echo "${runhub_chart_yaml}" | yq --exit-status '
   select(.kind == "ApplicationSet" and .metadata.name == "runhub").spec.generators.[] |
   select(.list).list.elements.[] |
   select(.metadata.name == "argo-cd").spec.sources.[] |
   select(.chart == "argo-cd")')"
-version="$(echo "${app_yaml}" | yq --exit-status '.targetRevision')"
-values="$(echo "${app_yaml}"  | yq --exit-status '.helm.values')"
-echo "${values}" | helm upgrade --install --create-namespace \
-  --namespace argocd argocd \
-  --repo https://argoproj.github.io/argo-helm argo-cd --version "${version}" \
-  --values - > /dev/null
+version="$(echo "${app_source_yaml}" | yq --exit-status '.targetRevision')"
+values="$(echo "${app_source_yaml}"  | yq --exit-status '.helm.values')"
+chart_yaml="$(echo "${values}" | helm template --namespace argocd argocd \
+  --repo https://argoproj.github.io/argo-helm argo-cd --version "${version}" --values -)"
+printf '%s\n%s\n' "${namespace_chart_yaml}" "${chart_yaml}" \
+  | kubectl apply --filename - > /dev/null
