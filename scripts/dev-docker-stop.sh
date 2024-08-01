cdfaba2196cd17a96d6d5a411f2ca78fcb2ff82d
Michael Gallagher <mjgallag@gmail.com>
2024-08-01 15:56:59 -0700

    feat: add dev docker stop script

---
+++ scripts/dev-docker-stop.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+scripts_dir="$(dirname "$0")"
+
+. "${scripts_dir}"/devbox-shellenv.sh
+echo 'Stopping dev runhub docker.'
+colima stop --profile dev-runhub
