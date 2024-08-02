1e51e704a3a34a1bb8393bc2a298bfb34b398c95
Michael Gallagher <mjgallag@gmail.com>
2024-08-02 14:14:00 -0700

    perf: only generate & eval devbox shellenv once

--- scripts/devbox-shellenv.sh
+++ scripts/devbox-shellenv.sh
@@ -5,6 +5,9 @@ set -o nounset
 
 runhub_dir="$(dirname "$0")"/..
 
-devbox_shellenv="$(nix run nixpkgs/6e14bbce7bea6c4efd7adfa88a40dac750d80100#devbox -- shellenv \
-  --config "${runhub_dir}" --init-hook --install --no-refresh-alias)"
-eval "${devbox_shellenv}"
+if ! "${RUNHUB_DEVBOX_SHELLENV:-false}"; then
+  devbox_shellenv="$(nix run nixpkgs/6e14bbce7bea6c4efd7adfa88a40dac750d80100#devbox -- shellenv \
+    --config "${runhub_dir}" --env RUNHUB_DEVBOX_SHELLENV=true \
+    --init-hook --install --no-refresh-alias)"
+  eval "${devbox_shellenv}"
+fi
