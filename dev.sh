fdc3707f029f06d9d1617ba8bdc949d013bca695
Michael Gallagher <mjgallag@gmail.com>
2024-08-05 13:44:50 -0700

    style: new lines only for new blocks

--- dev.sh
+++ dev.sh
@@ -4,5 +4,4 @@ set -o errexit
 set -o nounset
 
 scripts_dir="$(dirname "$0")"/scripts
-
 "${scripts_dir}"/install-nix.sh
--- scripts/dev-docker-stop.sh
+++ scripts/dev-docker-stop.sh
@@ -4,7 +4,6 @@ set -o errexit
 set -o nounset
 
 scripts_dir="$(dirname "$0")"
-
 . "${scripts_dir}"/devbox-shellenv.sh
 echo 'Stopping dev runhub docker.'
 colima stop --profile dev-runhub
