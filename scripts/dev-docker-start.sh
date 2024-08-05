0f4f20d949a691deb0d9d0c1fc8dd94b9f948a57
Michael Gallagher <mjgallag@gmail.com>
2024-08-05 08:57:58 -0700

    refactor: prepend host to colima version

--- scripts/dev-docker-start.sh
+++ scripts/dev-docker-start.sh
@@ -23,24 +23,24 @@ is_instance_config_equal() (
   fi
 )
 
-get_colima_version() (
-  colima_version_output="$(colima version)"
-  colima_version_head="$(echo "${colima_version_output}" | head -n 1)"
-  echo "${colima_version_head}" | cut -d ' ' -f 3
+get_host_colima_version() (
+  host_colima_version_output="$(colima version)"
+  host_colima_version_head="$(echo "${host_colima_version_output}" | head -n 1)"
+  echo "${host_colima_version_head}" | cut -d ' ' -f 3
 )
 
 main() (
   . "${scripts_dir}"/devbox-shellenv.sh
   echo 'Starting dev runhub docker.'
   dev_docker="$(get_dev_docker)"
-  colima_version="$(get_colima_version)"
+  host_colima_version="$(get_host_colima_version)"
   host_cpus="$(getconf _NPROCESSORS_CONF)"
   host_memory_gib="$("${scripts_dir}"/get-memory-gib.sh)"
   half_host_memory_gib="$(echo "${host_memory_gib}"' / 2' | bc)"
 
   if [ "${dev_docker}" ]; then
     is_colima_version_equal="$(is_instance_config_equal \
-      "${dev_docker}" 'env.RUNHUB_COLIMA_VERSION' "${colima_version}")"
+      "${dev_docker}" 'env.RUNHUB_COLIMA_VERSION' "${host_colima_version}")"
 
     if ! "${is_colima_version_equal}"; then
       colima delete --force --profile dev-runhub > /dev/null 2>&1
@@ -55,7 +55,7 @@ main() (
     fi
   fi
 
-  colima start --profile dev-runhub --env RUNHUB_COLIMA_VERSION="${colima_version}" \
+  colima start --profile dev-runhub --env RUNHUB_COLIMA_VERSION="${host_colima_version}" \
     --cpu "${host_cpus}" --memory "${half_host_memory_gib}"
 )
 
