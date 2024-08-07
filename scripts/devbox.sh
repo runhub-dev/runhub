086cb61bf47512ff67c1b75aecf4700ed25e5c76
Michael Gallagher <mjgallag@gmail.com>
2024-08-07 11:21:00 -0700

    refactor: extract devbox flake variable

--- scripts/devbox.sh
+++ scripts/devbox.sh
@@ -5,5 +5,6 @@ set -o nounset
 
 scripts_dir="$(dirname "$0")"
 runhub_dir="${scripts_dir}"/..
+devbox_flake='nixpkgs/6e14bbce7bea6c4efd7adfa88a40dac750d80100#devbox'
 "${scripts_dir}"/install-nix.sh
-nix run nixpkgs/6e14bbce7bea6c4efd7adfa88a40dac750d80100#devbox -- --config "${runhub_dir}" "$@"
+nix run "${devbox_flake}" -- --config "${runhub_dir}" "$@"
