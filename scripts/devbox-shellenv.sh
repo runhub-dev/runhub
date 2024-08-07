8bea75c4e92da60a28d75e5c57d34cd2c2792de3
Michael Gallagher <mjgallag@gmail.com>
2024-08-07 08:53:24 -0700

    perf: skip devbox shellenv if already evaluated

--- scripts/devbox-shellenv.sh
+++ scripts/devbox-shellenv.sh
@@ -4,10 +4,10 @@ set -o errexit
 set -o nounset
 
 runhub_dir="$(dirname "$0")"/..
+runhub_absolute_dir="$(cd "${runhub_dir}" && pwd)"
 
-if ! "${RUNHUB_DEVBOX_SHELLENV:-false}"; then
+if [ "${DEVBOX_PROJECT_ROOT:-}" != "${runhub_absolute_dir}" ]; then
   devbox_shellenv="$(nix run nixpkgs/6e14bbce7bea6c4efd7adfa88a40dac750d80100#devbox -- shellenv \
-    --config "${runhub_dir}" --env RUNHUB_DEVBOX_SHELLENV=true \
-    --init-hook --install --no-refresh-alias)"
+    --config "${runhub_dir}" --init-hook --install --no-refresh-alias)"
   eval "${devbox_shellenv}"
 fi
