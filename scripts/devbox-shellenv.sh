d5cbc4e4578ead34095afc74283e6e4f3c107755
Michael Gallagher <mjgallag@gmail.com>
2024-08-07 11:55:44 -0700

    fix: always eval devbox shellenv as it may change

--- scripts/devbox-shellenv.sh
+++ scripts/devbox-shellenv.sh
@@ -4,10 +4,5 @@ set -o errexit
 set -o nounset
 
 scripts_dir="$(dirname "$0")"
-runhub_dir="${scripts_dir}"/..
-runhub_absolute_dir="$(cd "${runhub_dir}" && pwd)"
-
-if [ "${DEVBOX_PROJECT_ROOT:-}" != "${runhub_absolute_dir}" ]; then
-  devbox_shellenv="$("${scripts_dir}"/devbox.sh shellenv --init-hook --install --no-refresh-alias)"
-  eval "${devbox_shellenv}"
-fi
+devbox_shellenv="$("${scripts_dir}"/devbox.sh shellenv --init-hook --install --no-refresh-alias)"
+eval "${devbox_shellenv}"
