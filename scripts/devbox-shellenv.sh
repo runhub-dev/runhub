1cd3b67c2d95509094268d852cbdcea3f3efafec
Michael Gallagher <mjgallag@gmail.com>
2024-08-08 16:16:56 -0700

    perf: skip devbox shellenv if devbox.lock unchanged

--- scripts/devbox-shellenv.sh
+++ scripts/devbox-shellenv.sh
@@ -4,5 +4,12 @@ set -o errexit
 set -o nounset
 
 scripts_dir="$(dirname "$0")"
+runhub_dir="${scripts_dir}"/..
+runhub_absolute_dir="$(cd "${runhub_dir}" && pwd)"
 
-"${scripts_dir}"/devbox.sh shellenv --init-hook --install --no-refresh-alias
+devbox_lock_cksum="$(cksum "${runhub_absolute_dir}"/devbox.lock)"
+
+if [ "${__RUNHUB_DEVBOX_LOCK_CKSUM:-}" != "${devbox_lock_cksum}" ]; then
+  "${scripts_dir}"/devbox.sh shellenv --init-hook --install --no-refresh-alias \
+    --env __RUNHUB_DEVBOX_LOCK_CKSUM="${devbox_lock_cksum}"
+fi
