0fbfe956d878d0f11235313c391c82f1264fa2f3
Michael Gallagher <mjgallag@gmail.com>
2024-11-22 13:43:35 -0800

    build: add devbox 0.13.6 (nixpkgs/34a6264)

---
+++ scripts/install-upgrade-devbox.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+scripts_dir="$(dirname "$0")"
+
+minimum_required_version='0.13.6'
+nixpkgs_commit='34a626458d686f1b58139620a8b2793e9e123bba'
+
+is_installed_upgraded() {
+  if command -v devbox > /dev/null; then
+    installed_version="$(devbox version)"
+    "${scripts_dir}"/is-version-greater-equal.sh "${installed_version}" "${minimum_required_version}"
+  else
+    echo 'no'
+  fi
+}
+
+install_upgrade() {
+  nix profile remove --quiet --quiet devbox
+  nix profile install --override-flake nixpkgs github:NixOS/nixpkgs/"${nixpkgs_commit}" nixpkgs#devbox
+}
+
+main() {
+  . "${scripts_dir}"/run-nix-daemon.sh
+  is_installed_upgraded="$(is_installed_upgraded)"
+
+  if [ "${is_installed_upgraded}" = 'no' ]; then
+    "${scripts_dir}"/confirm.sh \
+      'Install/Upgrade Devbox v'"${minimum_required_version}"' with `nix profile`?'
+    install_upgrade
+    was_installed_upgraded="$(is_installed_upgraded)"
+
+    if [ "${was_installed_upgraded}" = 'no' ]; then
+      echo 'Devbox v'"${minimum_required_version}"' or higher is required.'
+      exit 1
+    fi
+  fi
+}
+
+main "$@"
