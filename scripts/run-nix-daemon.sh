26c6dc3833c52c8b3d9131d679cd12cb7148ddb1
Michael Gallagher <mjgallag@gmail.com>
2024-12-17 08:30:07 -0800

    build: add nix 2.25.3 (installer 0.32.1)

---
+++ scripts/run-nix-daemon.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+nix_daemon_script='/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+
+if ! command -v nix >/dev/null && [ -e "${nix_daemon_script}" ]; then
+  . "${nix_daemon_script}"
+fi
