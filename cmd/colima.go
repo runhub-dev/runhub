a56ca172a8313804fd6df7b8bac736cb9bfc4e39
Michael Gallagher <mjgallag@gmail.com>
2025-10-06 07:16:46 -0700

    refactor: strip colima command to minimum

--- cmd/colima.go
+++ cmd/colima.go
@@ -21,16 +21,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// colimaCmd represents the colima command
 var colimaCmd = &cobra.Command{
-	Use:   "colima",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use: "colima",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("colima called")
 	},
@@ -38,14 +30,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(colimaCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// colimaCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// colimaCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
